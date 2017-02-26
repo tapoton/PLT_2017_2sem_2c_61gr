3f6e044deb8cb3287eac85ec668d0151653402a2
Irina <malezhina.irina@yandex.ru>
2017-02-26 16:36:34 +0300

    7 задача

---
+++ laba1.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+
+struct trie 
+{ 
+	char *value;
+ 	char ch; 
+ 	trie *sibling;    
+ 	trie *child; 
+};
+
+trie *trie_create() 
+{
+	trie *node = new trie;
+	node->ch = '\0'; 
+	node->value = NULL; 
+	node->sibling = NULL; 
+	node->child = NULL;
+	return node;        
+}
+
+void trie_lookup(trie *root, int val, int &numsg) //подсчет кол-ва слов, содержащих определенное кол-во согласных и вывод их на экран
+{
+	trie *node, *list;
+	for (node = root; node != NULL; node = node->sibling) 
+	{
+		if (node->value != NULL) 
+		{  
+			int ivalue = atoi(node->value);
+			if (ivalue == val)
+				numsg++;
+		}    
+		if (node->child != NULL) 
+			trie_lookup(node->child, val, numsg);	
+	}
+}
+struct trie *trie_insert(trie *root, char *key, char *value) // вставка введеного слова, в качестве ключа - количество согласных букв в слове 
+{ 
+	trie *node, *parent, *list;
+	parent = NULL; 
+	list = root; 
+	for (; *key != '\0'; key++) 
+	{ 
+		for (node = list; node != NULL; node = node->sibling) 
+		{
+			 if (node->ch == *key) 
+			 	break; 
+		}
+		if (node == NULL) 
+		{ 
+			node = trie_create();
+			node->ch = *key; 
+			node->sibling = list; 
+			if (parent != NULL) 
+				parent->child = node; 
+			else 
+				root = node; 
+			list = NULL; 
+		} 
+		else 
+		{ 
+			list = node->child; 
+		} 
+		parent = node;
+	}  
+	if (node->value != NULL) 
+		free(node->value); 
+	node->value = strdup(value); 
+	return root;
+}
+
+void trie_print(trie *root, int level) //вывод дерева
+{
+	 trie *node; int i;
+	 for (node = root; node != NULL; node = node->sibling) 
+	 { 
+	 	for (i = 0; i < level; i++)
+		 	 cout<< ' ';
+		if (node->value != NULL) 
+			cout<<'\n'<< node->ch <<' '<< node->value; 
+		else 
+			cout<< '\n'<< node->ch;     
+		if (node->child != NULL) 
+			trie_print(node->child, level + 1);
+	}
+}
+int consonants(char *str) // подсчет количества согласных введенном слове 
+{
+	int sg=0;
+    char sog[]="Ѕб¬в√гƒд∆ж«з…й кЋлћмЌнѕп–р—с“т‘ф’х÷ц„чЎшўщ";
+ 
+    for(int i=0; i < strlen(str); i++)
+            for(int j=0; j < strlen(sog); j++)
+                    if(str[i] ==sog[j])
+                            sg++;
+    
+ 	return sg;
+}
+
+int main() 
+{ 
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);	
+	
+	trie *root = NULL;
+	
+	int otv1, otv2;
+	do
+	{	
+			cout << " ¬ыберите действие:" << endl << "1. ƒобавить новое слово" << endl << "2. ¬ывести дерево"<< endl
+			<< "3. ќпределить кол-во слов, которые содержат определенное кол-во согласных"<< endl << "0. ¬ыход"<< endl <<"=";
+			cin >> otv1;
+			switch (otv1)
+			{
+				case 0: 
+					break;
+				case 1:  
+					{
+						cout<< "¬ведите слово:" << endl;
+						char str[100];
+ 						cin >> str;
+ 						char sog[10];
+ 						int kol = consonants(str);
+ 						sprintf(sog, "%d", kol);
+						root = trie_insert(root, str, sog); 
+						break;
+					}
+				case 2: 
+					{
+						trie_print(root, 0);
+						cout<< endl;
+						break;
+					}
+				case 3:
+					{
+						int ksg, numsg=0;
+						cout << "¬ведите кол-во согласных:";
+						cin >> ksg;
+						trie_lookup(root, ksg, numsg);
+						cout << "“аких слов:" << numsg << endl;
+						break;
+					}
+					
+				default:
+					{
+						cout << endl << "ќшибка" << endl; 
+						break;
+					}
+			}
+		
+	}
+	while(otv1 != 0);
+	
+	system("pause");
+	return 0;
+}
+
