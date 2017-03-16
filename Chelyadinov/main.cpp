dddb9b23c5f1add9132f2b727a7073a932787208
vanyc777 <vanyc777@yandex.ru>
2017-03-16 17:43:32 +0300

    Add files via upload

--- Chelyadinov/main.cpp
+++ Chelyadinov/main.cpp
@@ -1,6 +1,6 @@
-﻿
- //Для заданного текста подсчитать кол-во слов состоящих только из согласных букв, используя trie деревья
- 
+
+//Для заданного текста подсчитать колво слов состоящих только из согласных букв, используя trie деревья
+
 #include <set>
 #include <iostream>
 #include <cstdlib>
@@ -23,31 +23,27 @@ private:
             q->Liter[i] = NULL;
         }
     }
-    void Delete_trie(TRIE* q)
-    {
-   /* for(int i=0; i<26; i++)                //крашится на комбинациях "kot kit ktr ktra","tar tir tdj tdje" и т.п. 
-                                            //на других кобинациях работает
-        {                                    //в чем проблема не выяснил
-        if(q->Liter[i]!=NULL){
-            Delete_trie(q->Liter[i]);
-            TRIE *p=q;
-            q=NULL;
-            delete p;
-            
-        }}*/
+
+    void Delete_trie(TRIE* q) {
+        for (int i = 0; i < 26; i++)
+ {
+            if (q->Liter[i] != NULL) {
+                Delete_trie(q->Liter[i]);
+            }
+        }
+        TRIE *p = q;
+        delete p;
     }
-
-
 public:
 
     int GetKol() {
         return kol;
     }
-    ~TRIE_TREE(){
-      Delete_trie(begin); 
-        
+
+    ~TRIE_TREE() {
+        Delete_trie(begin);
     }
-    
+
     TRIE_TREE() {
         Vowels.insert('a');
         Vowels.insert('e');
@@ -61,7 +57,8 @@ public:
     }
     void Add_words(string);
     void Without_vowels(TRIE*);
-    void Vivod_trie_tree(TRIE*,int);
+    void Vivod_trie_tree(TRIE*, int);
+
     TRIE* GetTrie() {
         return begin;
     }
@@ -71,31 +68,32 @@ int main() {
 
     TRIE_TREE A;
     string s;
-    cout<<"Vvedite slova cherez probel: \n";
+    cout << "Vvedite slova cherez probel: \n";
     getline(cin, s);
     A.Add_words(s);
-    cout<<"Vivod dereva:\n";
-    A.Vivod_trie_tree(A.GetTrie(),0);
+    cout << "Vivod dereva:\n";
+    A.Vivod_trie_tree(A.GetTrie(), 0);
     A.Without_vowels(A.GetTrie());
-    cout<<"Kolichestvo slov bez glasnih= ";
+    cout << "Kolichestvo slov bez glasnih= ";
     cout << A.GetKol();
     return 0;
 }
-void TRIE_TREE::Vivod_trie_tree(TRIE* q, int a)
-{
-for (int i = 0; i < 26; i++) {
 
-            if (q->Liter[i] != NULL)
-            {
-                for(int j=0; j<a; j++)
-                    cout<<' ';
-            
-                cout<<(char)('a'+i);
-            if(q->Liter[i]->end_word) cout<<' '<<"(end)";
-                cout<<endl;
-             Vivod_trie_tree(q->Liter[i],a+1);}
-                 }   
+void TRIE_TREE::Vivod_trie_tree(TRIE* q, int a) {
+    for (int i = 0; i < 26; i++) {
+
+        if (q->Liter[i] != NULL) {
+            for (int j = 0; j < a; j++)
+                cout << ' ';
+
+            cout << (char) ('a' + i);
+            if (q->Liter[i]->end_word) cout << ' ' << "(end)";
+            cout << endl;
+            Vivod_trie_tree(q->Liter[i], a + 1);
+        }
+    }
 }
+
 void TRIE_TREE::Add_words(string s) {
     int i = 0;
     TRIE *q = begin;
@@ -122,13 +120,13 @@ void TRIE_TREE::Add_words(string s) {
 void TRIE_TREE::Without_vowels(TRIE* q) {
     if (q->end_word) {
         kol++;
-    } 
-        for (int i = 0; i < 26; i++) {
+    }
+    for (int i = 0; i < 26; i++) {
 
-            if (q->Liter[i] != NULL)
+        if (q->Liter[i] != NULL)
 
-                if (!Vowels.count('a' + i)) {
-                    Without_vowels(q->Liter[i]);
-                }
-        }
+            if (!Vowels.count('a' + i)) {
+                Without_vowels(q->Liter[i]);
+            }
+    }
 }
